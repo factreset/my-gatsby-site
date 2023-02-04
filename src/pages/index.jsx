19b6c051c1a991f09a1b2c48f5e67845d3bb4662
Delon O'Donnell <delonodonnell@gmail.com>
2023-02-04 13:29:36 +0800

    Finished Gatsby Tutorial Pt. 3

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,17 @@
 // Step 1: Import React
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="House of Mine">
       <p>Nahahahahahaahahh I'm home honeybunch</p>
+      <StaticImage
+        src="../images/greth-the-undying.png"
+        alt="Greth the Undying"
+      />
     </Layout>
   );
 };
