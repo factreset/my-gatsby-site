9b149c565df4e025d168a160d6ef643b28725a05
Delon O'Donnell <delonodonnell@gmail.com>
2023-02-05 14:05:06 +0800

    Finished Gatsby Tutorial Pt. 5

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -2,15 +2,19 @@ import * as React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import { graphql } from "gatsby";
+// import
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ol>
-        {data.allFile.nodes.map((node) => (
-          <li>{node.name}</li>
-        ))}
-      </ol>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+        </article>
+      ))}
+
       <p>My cool posts will go in here</p>
     </Layout>
   );
@@ -18,9 +22,14 @@ const BlogPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        excerpt
+        id
       }
     }
   }
